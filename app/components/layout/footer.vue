521b8edf3da6b1241ec48e96350e6987dc4c6456
charlyrm14 <charlyrm14@gmail.com>
2025-08-07 17:24:02 -0600

    feature: seccion perfil

--- app/components/layout/footer.vue
+++ app/components/layout/footer.vue
@@ -28,10 +28,13 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                 </svg>
         </NuxtLink>
-        <button class="flex flex-col items-center py-2 px-4 text-white rounded-4xl cursor-pointer hover:opacity-75">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 md:size-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-            </svg>
-        </button>
+        <NuxtLink 
+            to="profile"
+            class="flex flex-col items-center py-2 px-4 text-white rounded-4xl cursor-pointer hover:opacity-75"
+            :class="route.path === '/profile' ? 'bg-red-500 ' : ''">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 md:size-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                </svg>
+        </NuxtLink>
     </footer>
 </template>
---
+++ app/components/user/ChangeTheme.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+    const showThemeContent = ref<boolean>(false)
+</script>
+
+<template>
+    <div class="border-b border-gray-300 dark:border-dark-extralight">
+        <div class="flex justify-between items-center">
+            <div class="flex justify-start items-center gap-x-4 mb-3">
+                <div class="dark:text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42" />
+                    </svg>
+                </div>
+                <div>
+                    <h4 class="dark:text-white text-base"> Tema </h4>
+                    <span class="dark:text-white text-xs md:text-sm font-extralight"> Administra el color de la pantalla </span>
+                </div>
+            </div>
+            <button 
+                @click="showThemeContent = !showThemeContent"
+                class="bg-white dark:bg-dark-extralight border border-gray-200 dark:border-none p-1.5 rounded-full dark:text-white cursor-pointer hover:opacity-75">
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        fill="none"
+                        viewBox="0 0 24 24" 
+                        stroke-width="1.5" 
+                        stroke="currentColor" 
+                        class="size-6">
+                            <path 
+                                stroke-linecap="round" 
+                                stroke-linejoin="round" 
+                                :d="showThemeContent ? 'm4.5 15.75 7.5-7.5 7.5 7.5' : 'm19.5 8.25-7.5 7.5-7.5-7.5'"/>
+                    </svg>
+            </button>
+        </div>
+        <div 
+            v-if="showThemeContent"
+            class="flex justify-start items-center mt-6 mb-3 gap-x-6">
+                <div class="bg-white border border-gray-200 p-4 rounded-lg flex justify-start gap-x-3 cursor-pointer hover:opacity-75 shadow-lg">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                        </svg>
+                    </div>
+                    <div>
+                        <p class="font-light"> Tema claro </p>
+                    </div>
+                </div>
+                <div class="bg-dark dark:bg-dark-extralight border-2 border-blue-500 p-4 rounded-lg flex justify-start gap-x-3 cursor-pointer hover:opacity-75 shadow-lg">
+                    <div class="text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                        </svg>
+                    </div>
+                    <div>
+                        <p class="font-light text-white"> Tema obscuro </p>
+                    </div>
+                </div>
+        </div>
+    </div>
+</template>
