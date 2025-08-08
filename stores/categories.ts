8a7c0fbcddaf31afed8f610b5fc9e3de3a94ab72
charlyrm14 <charlyrm14@gmail.com>
2025-08-08 17:51:56 -0600

    feature: listado de categorías

---
+++ stores/categories.ts
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia'
+import type { Category } from "~~/types/Product";
+import type { Pagination } from "~~/types/Pagination";
+import type { ApiResponse } from "~~/types/ApiResponse";
+
+export const useCategoriesStore = defineStore('categories', () => {
+
+    const config = useRuntimeConfig();
+    const categories = ref<Pagination<Category> | null>(null);
+
+    const fetchGetCategories = async(page = 1) => {
+        try {
+
+            const response = await $fetch<ApiResponse<Pagination<Category>>>(
+                `${config.public.apiBaseUrl}/categories`
+            );
+
+            categories.value = response.data;
+            
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return {
+        categories,
+        fetchGetCategories
+    }
+})
+    
