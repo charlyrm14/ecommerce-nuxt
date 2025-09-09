b912e4e5b3b2b31f3ab3bf687499b73288f93db7
charlyrm14 <charlyrm14@gmail.com>
2025-09-09 17:36:37 -0600

    featureadd-product-to-favorite): se agrega funcionalidad para agregar producto a favoritos

--- app/pages/favorites/index.vue
+++ app/pages/favorites/index.vue
@@ -1,6 +1,13 @@
 <script setup>
     import FavoriteItem from '~/components/user/favorites/FavoriteItem.vue';
     import SectionHeader from '~/components/user/layouts/SectionHeader.vue';
+    import { useFavoritesStore } from '~~/stores/favorites';
+    import Heart from '~/assets/media/chat-bubble.png'
+
+    const favoritesStore = useFavoritesStore()
+
+    onMounted(favoritesStore.getFavorites)
+
 </script>
 
 <template>
@@ -8,8 +15,24 @@
         title="Favoritos"/>
 
     <section class="mt-10 px-4">
-        <div class="grid grid-cols-2 md:grid-cols-4 gap-3">
-            <FavoriteItem/>
+        <div
+            v-if="favoritesStore.favorites.length > 0"
+            class="grid grid-cols-2 md:grid-cols-4 gap-3">
+            <FavoriteItem
+                v-for="favorite in favoritesStore.favorites"
+                :key="favorite.id"
+                :favorite="favorite"/>
+        </div>
+        <div v-else>
+            <div class="flex justify-center items-center">
+                <img 
+                    :src="Heart" 
+                    alt="Heart" 
+                    class="w-80 drop-shadow-2xl"/>
+            </div>
+            <h1 class="text-red-500 text-center text-base font-light mt-8">
+                Aún no tienes productos en tu lista de favoritos. ¡Explora nuestro catálogo y guarda los que más te gusten!
+            </h1>
         </div>
     </section>
     
