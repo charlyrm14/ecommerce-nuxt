80c4b8421618ace6982831e008196e5c05dc2f52
charlyrm14 <charlyrm14@gmail.com>
2025-09-13 13:48:23 -0600

    feature(ui-select-main-image): se agrega componente visual para mostrar galeria de archivos subidos

--- app/components/admin/products/UploadProductFiles.vue
+++ app/components/admin/products/UploadProductFiles.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
     import FileUploader from '~/components/common/FileUploader.vue';
-
+    import FileGallery from '~/components/common/FileGallery.vue';
 </script>
 
 <template>
     <div class="mt-7 rounded-lg border border-gray-200 bg-white p-4">
         <div class="mb-3">
-            <h2 class="font-light text-gray-500"> 
-                Sube tus fotos para destacar sus características y generar más interés entre tus compradores
+            <h2 class="font-light text-gray-400"> 
+                Agrega fotos a tu producto para destacar sus características y generar más interés entre tus compradores
             </h2>
         </div>
 
@@ -18,5 +18,8 @@
             :maxFiles="5"
             :maxSizeMB="5"
             :concurrency="1"/>
+
+        <FileGallery/>
+
     </div>
 </template>
