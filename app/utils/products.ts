5d4a89c184fda24e70ec66a062912e842ce1b150
charlyrm14 <charlyrm14@gmail.com>
2025-09-13 20:40:46 -0600

    feature(select-product-images): se agregan funcionalidad para elegir una imagen como principal de un producto

--- app/utils/products.ts
+++ app/utils/products.ts
@@ -1,5 +1,12 @@
 import ProductDescription from "~/components/user/products/ProductDescription.vue"
 import ProductReviews from "~/components/user/products/ProductReviews.vue"
+import { useProductsStore } from "~~/stores/products"
+import { useFilesStore } from "~~/stores/files"
+
+type PayloadImages = {
+    id: number
+    is_main: boolean
+}
 
 export const sectionTabs = () => {
 
@@ -7,4 +14,30 @@ export const sectionTabs = () => {
         { id: 1, title: 'Información', key: ProductDescription},
         { id: 2, title: 'Opiniones', key: ProductReviews},
     ]
+}
+
+export const payloadImages = (): PayloadImages[] => {
+
+    const productsStore = useProductsStore()
+    const filesStore = useFilesStore()
+
+    /* There are uploaded files and the user select a main image */
+    if (productsStore.form.images.length > 0) {
+        return productsStore.form.images
+    }
+
+    /* 
+    * There are files uploaded but the user has not selected a main image 
+    * and the first uploaded image is assigned as the main image.
+    */
+    if(filesStore.files?.[0]?.length) { 
+
+        return filesStore.files[0].map((file, index) => ({
+            id: file.id,
+            is_main: index === 0
+        }))
+    }
+    
+    // Are not uploaded files
+    return [];
 }
