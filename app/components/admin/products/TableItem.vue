884c87bcfb09fe0482fcd3a12b0dc98ebfe84b76
charlyrm14 <charlyrm14@gmail.com>
2025-09-17 11:47:02 -0600

    feature(product-images-dashboard): se agrega imagen de producto en listado de productos

--- app/components/admin/products/TableItem.vue
+++ app/components/admin/products/TableItem.vue
@@ -3,15 +3,21 @@
     import type { Product } from '~~/types/Product';
     import { formatCurrency } from '#imports';
 
-    defineProps<{
+    const props = defineProps<{
         product: Product
     }>()
 
     const menuActions = ref<boolean>(false)
 
+    const config = useRuntimeConfig();
+
     const emit = defineEmits<{
         (e: 'openDeleteProduct', product: Product): void
     }>()
+
+    const thumbnail = computed(() => {
+        return props?.product?.files?.filter(file => file.variant === 'thumbnail')
+    })
 
 </script>
 
@@ -19,9 +25,21 @@
     <tr class="border-b border-gray-100">
         <td class="px-4 py-3">
             <div class="flex items-start gap-x-2">
-                <div class="bg-gray-100 border border-gray-200 rounded-lg text-gray-600 p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                <div class="bg-gray-100 border border-gray-200 rounded-lg text-gray-600 p-2">
+                    <img 
+                        v-if="thumbnail?.[0]"
+                        :src="`${config.public.storageBase}/${thumbnail?.[0]?.file_path}`" 
+                        :alt="`${product?.name}-${product?.id}`"
+                        class="w-8">
+                    <svg
+                        v-else 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke-width="1.5" 
+                        stroke="currentColor" 
+                        class="size-8">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                     </svg>
                 </div>
                 <div>
